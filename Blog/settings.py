aa6b3b4410193014da4def1d816c4a757aaa3c18
Chibuike Akpulu <akpuluchibuike3@gmail.com>
2022-06-29 03:44:48 +0100

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -87,17 +87,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-    },
-
-    'postgres': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'demo',
-        'USER': 'postgres',
-        'PASSWORD': 'djangoDB',
-        'HOST': 'localhost',
-        'PORT': '',
     }
-
 }
 
 
